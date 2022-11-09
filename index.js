1fa8113ed92cc2b776dfe5a47100b50261e6c16d
ronycse16b <rony.cse16b@gmail.com>
2022-11-09 22:54:29 +0600

    added .env

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+ require('dotenv').config();
 
 
 // midelware.config
 app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASSWORD}@cluster0.qnzupqp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
